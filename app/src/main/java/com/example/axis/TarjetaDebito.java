5409975eb179a60a3ba5ccbb5a790c501a33e180
Carlos Hernandez <soycarlosluis862@gmail.com>
2019-10-20 11:11:00 -0400

    Integracion con toolbar 2.0

--- app/src/main/java/com/example/axis/TarjetaDebito.java
+++ app/src/main/java/com/example/axis/TarjetaDebito.java
@@ -1,19 +1,43 @@
 package com.example.axis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class TarjetaDebito extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarjeta_debito);
+        setUpToolbar();
     }
 
+    private void setUpToolbar() {
+        toolbar = findViewById(R.id.toolbarmenu);
+        setSupportActionBar(toolbar);
+        showHomeUpIcon();
+    }
+    private void showHomeUpIcon() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent siguiente = new Intent(this,DashboardDeUsuario.class);
+                startActivity(siguiente);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     //Metodo para regresar
     public void regresar(View view){
