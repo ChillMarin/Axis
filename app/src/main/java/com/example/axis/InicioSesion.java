2ecb0281e86485daee268fff81e28db16323543a
marcelmarin <33253860+marcelmarin@users.noreply.github.com>
2019-10-20 18:32:05 -0400

    Se agrego el swipe up y todo lo demas

---
+++ app/src/main/java/com/example/axis/InicioSesion.java
@@ -0,0 +1,67 @@
+package com.example.axis;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
+
+public class InicioSesion extends AppCompatActivity {
+
+    public static final long DURATION_TRANSITION = 1000;
+
+    private GestureDetectorCompat gestureDetectorCompat = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+
+
+        DirectSwipeGestureListener gestureListener = new DirectSwipeGestureListener();
+        gestureListener.setActivity(this);
+        gestureDetectorCompat = new GestureDetectorCompat(this, gestureListener);
+    }
+
+
+
+
+    private Transition transition;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetectorCompat.onTouchEvent(event);
+        return true;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void onSlideClicked(){
+        transition = new Slide(Gravity.TOP );
+        iniciarActividadSecundaria();
+    }
+
+    @SuppressWarnings("unchecked")
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void iniciarActividadSecundaria(){
+        transition.setDuration(DURATION_TRANSITION);
+        transition.setInterpolator(new DecelerateInterpolator());
+
+        getWindow().setExitTransition(transition);
+
+        Intent intent = new Intent(this, DashboardDeUsuario.class);
+        startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle());
+
+    }
+}
